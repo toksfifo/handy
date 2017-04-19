36cfc417de627551b48419d2936b994ec2371df2
Toks Fifo <toksfifo@gmail.com>
2017-04-19 17:11:42 -0400

    tile factory

--- app.js
+++ app.js
@@ -7,26 +7,46 @@ function trigger(e) {
     const handy = $(`<div><p>${selectedText}</p></div>`)
       .addClass("handy");
 
-    const wikipediaHref = `https://en.wikipedia.org/w/index.php?search=${selectedText}`
-    const wikipedia = $(`<div><a target='_blank' href='${wikipediaHref}'>wikipedia</a></div>`);
+    const tileFactory = new TileFactory(selectedText);
 
-    const twitterHref = `https://twitter.com/search?q=${selectedText}`
-    const twitter = $(`<div><a target='_blank' href='${twitterHref}'>twitter</a></div>`);
+    const wikipedia = tileFactory.createTile("Wikipedia", "https://en.wikipedia.org/w/index.php?search=");
+    const twitter = tileFactory.createTile("Twitter", "https://twitter.com/search?q=");
+    const google = tileFactory.createTile("Google", "https://google.com/search?q=");
+    const googleMaps = tileFactory.createTile("Google Maps", "https://google.com/maps/search/");
+    const youtube = tileFactory.createTile("Youtube", "https://www.youtube.com/results?search_query=");
+    const spotify = tileFactory.createTile("Spotify", "https://open.spotify.com/search/results/");
+    const amazon = tileFactory.createTile("Amazon", "https://www.amazon.com/s/field-keywords=");
+    const ebay = tileFactory.createTile("eBay", "http://www.ebay.com/sch/&_nkw=");
+    const linkedin = tileFactory.createTile("Linkedin", "https://www.linkedin.com/search/results/index/?keywords=");
+    const netflix = tileFactory.createTile("Netflix", "https://www.netflix.com/search?q=");
+    const stackoverflow = tileFactory.createTile("Stack Overflow", "http://stackoverflow.com/search?q=");
+    const pinterest = tileFactory.createTile("Pinterest", "https://www.pinterest.com/search/pins/?q=");
+    const rt = tileFactory.createTile("Rotten Tomatoes", "https://www.rottentomatoes.com/search/?search=");
+    const imdb = tileFactory.createTile("IMDb", "http://www.imdb.com/find?q=");
 
-    const googleHref = `https://google.com/search?q=${selectedText}`
-    const google = $(`<div><a target='_blank' href='${googleHref}'>google</a></div>`);
+    const tiles = [
+      wikipedia,
+      twitter,
+      google,
+      googleMaps,
+      youtube,
+      spotify,
+      amazon,
+      ebay,
+      linkedin,
+      netflix,
+      stackoverflow,
+      pinterest,
+      rt,
+      imdb,
+    ];
 
-    const googleMapsHref = `https://google.com/maps/search/${selectedText}`
-    const googleMaps = $(`<div><a target='_blank' href='${googleMapsHref}'>google maps</a></div>`);
+    // const youtubeSampleHref = `<iframe width="560" height="315" src="https://www.youtube.com/embed/Qp_QA8ijWOE" frameborder="0" allowfullscreen></iframe>`
+    // const youtubeSample = $(`<div>${youtubeSampleHref}'</div>`);
 
-    const youtubeSampleHref = `<iframe width="560" height="315" src="https://www.youtube.com/embed/Qp_QA8ijWOE" frameborder="0" allowfullscreen></iframe>`
-    const youtubeSample = $(`<div>${youtubeSampleHref}'</div>`);
-
-    handy.append(wikipedia);
-    handy.append(twitter);
-    handy.append(google);
-    handy.append(googleMaps);
-    handy.append(youtubeSample);
+    for (let tile of tiles) {
+      handy.append(tile);
+    }
 
     $(body).append(handy);
   }
@@ -43,3 +63,15 @@ function getSelectionText() {
 }
 
 document.addEventListener("keyup", trigger);
+
+
+class TileFactory {
+  constructor(text) {
+    this.text = text;
+  }
+
+  createTile(displayName, baseHref) {
+    const href = `${baseHref}${this.text}`;
+    return $(`<div><a target='_blank' href='${href}'>${displayName}</a></div>`);
+  }
+}
